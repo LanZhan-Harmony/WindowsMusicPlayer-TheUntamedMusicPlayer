fc047bf27fb5a87f45ed259332b3ec6a52f30631
LanZhan <WANGYIYI06300602@126.com>
2024-11-18 23:03:10 +0800

    2024/11/18

--- The Untamed Music Player/Views/ShellPage.xaml.cs
+++ The Untamed Music Player/Views/ShellPage.xaml.cs
@@ -47,8 +47,8 @@ public sealed partial class ShellPage : Page
 
         TitleBarHelper.UpdateTitleBar(RequestedTheme);
 
-        KeyboardAccelerators.Add(BuildKeyboardAccelerator(VirtualKey.Left, VirtualKeyModifiers.Menu));
-        // KeyboardAccelerators.Add(BuildKeyboardAccelerator(VirtualKey.GoBack));
+        //KeyboardAccelerators.Add(BuildKeyboardAccelerator(VirtualKey.Left, VirtualKeyModifiers.Menu));
+        //KeyboardAccelerators.Add(BuildKeyboardAccelerator(VirtualKey.GoBack));
 
     }
 
