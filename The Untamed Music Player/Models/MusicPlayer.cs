1da015a2f1a768b347f553d62c5527dc3f91ae26
LanZhan <WANGYIYI06300602@126.com>
2025-01-23 10:28:06 +0800

    2025/1/23(2)

--- The Untamed Music Player/Models/MusicPlayer.cs
+++ The Untamed Music Player/Models/MusicPlayer.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Collections.ObjectModel;
+﻿using System.Collections.ObjectModel;
 using System.ComponentModel;
-using System.Diagnostics;
-using System.Runtime.InteropServices.WindowsRuntime;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Input;
--- The Untamed Music Player/ViewModels/MainViewModel.cs
+++ The Untamed Music Player/ViewModels/MainViewModel.cs
@@ -1,5 +1,4 @@
-﻿using System.ComponentModel;
-using CommunityToolkit.Mvvm.ComponentModel;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
 using Microsoft.UI;
 using Microsoft.UI.Composition;
 using Microsoft.UI.Composition.SystemBackdrops;
