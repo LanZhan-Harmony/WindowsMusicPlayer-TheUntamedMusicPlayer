b3e432fac3930c50b8281b2ddd6aeb16d33e2a5a
LanZhan <WANGYIYI06300602@126.com>
2025-03-06 23:24:35 +0800

    2025/3/6(2)

--- The Untamed Music Player/Contracts/Models/IOnlineMusicInfo.cs
+++ The Untamed Music Player/Contracts/Models/IOnlineMusicInfo.cs
@@ -1,9 +1,26 @@
+using Microsoft.UI;
+using Microsoft.UI.Xaml.Media;
+
 namespace The_Untamed_Music_Player.Contracts.Models;
 public interface IBriefOnlineMusicInfo : IBriefMusicInfoBase
 {
     bool IsAvailable { get; set; }
     long ID { get; set; }
     long AlbumID { get; set; }
+
+    new SolidColorBrush GetTextForeground(IDetailedMusicInfoBase? currentMusic, bool isDarkTheme)
+    {
+        var defaultColor = isDarkTheme ? Colors.White : Colors.Black;
+
+        if (currentMusic is not null && currentMusic.IsOnline && ID == ((IDetailedOnlineMusicInfo)currentMusic).ID)
+        {
+            var highlightColor = isDarkTheme
+                ? ColorHelper.FromArgb(0xFF, 0x42, 0x9C, 0xE3)
+                : ColorHelper.FromArgb(0xFF, 0x00, 0x5A, 0x9E);
+            return new SolidColorBrush(highlightColor);
+        }
+        return new SolidColorBrush(defaultColor);
+    }
 }
 
 public interface IDetailedOnlineMusicInfo : IBriefOnlineMusicInfo, IDetailedMusicInfoBase
