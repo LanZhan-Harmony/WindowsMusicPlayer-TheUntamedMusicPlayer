62b77ce5b3428bb7b9b702c519d3e9fe78551992
LanZhan <WANGYIYI06300602@126.com>
2025-08-08 22:33:08 +0800

    更改字体

--- The Untamed Music Player/Views/PlayQueuePage.xaml.cs
+++ The Untamed Music Player/Views/PlayQueuePage.xaml.cs
@@ -73,23 +73,41 @@ public sealed partial class PlayQueuePage : Page
     private void Grid_PointerEntered(object sender, PointerRoutedEventArgs e)
     {
         var grid = sender as Grid;
-        var checkBox = grid?.FindName("ItemCheckBox") as CheckBox;
-        var playButton = grid?.FindName("PlayButton") as Button;
-        var fontIcon = grid?.FindName("MusicFontIcon") as FontIcon;
-        checkBox?.Visibility = Visibility.Visible;
-        playButton?.Visibility = Visibility.Visible;
-        fontIcon?.Visibility = Visibility.Collapsed;
+        (grid?.FindName("ItemCheckBox") as CheckBox)?.Visibility = Visibility.Visible;
+        (grid?.FindName("PlayButton") as Button)?.Visibility = Visibility.Visible;
+        (grid?.FindName("MusicFontIcon") as FontIcon)?.Visibility = Visibility.Collapsed;
+        (grid?.FindName("PlayingFontIcon") as FontIcon)?.Visibility = Visibility.Collapsed;
     }
 
     private void Grid_PointerExited(object sender, PointerRoutedEventArgs e)
     {
         var grid = sender as Grid;
-        var checkBox = grid?.FindName("ItemCheckBox") as CheckBox;
-        var playButton = grid?.FindName("PlayButton") as Button;
-        var fontIcon = grid?.FindName("MusicFontIcon") as FontIcon;
-        checkBox?.Visibility = Visibility.Collapsed;
-        playButton?.Visibility = Visibility.Collapsed;
-        fontIcon?.Visibility = Visibility.Visible;
+        (grid?.FindName("ItemCheckBox") as CheckBox)?.Visibility = Visibility.Collapsed;
+        (grid?.FindName("PlayButton") as Button)?.Visibility = Visibility.Collapsed;
+        (grid?.FindName("MusicFontIcon") as FontIcon)?.Visibility = Visibility.Visible;
+        if (
+            grid?.FindName("PlayingFontIcon") is FontIcon playingFontIcon
+            && grid.DataContext is IBriefSongInfoBase songInfo
+        )
+        {
+            var currentSong = Data.MusicPlayer.CurrentSong;
+            var isCurrentlyPlaying = currentSong switch
+            {
+                null => false,
+                _ when currentSong.IsOnline && songInfo is IBriefOnlineSongInfo onlineSong => (
+                    (IDetailedOnlineSongInfo)currentSong
+                ).ID == onlineSong.ID
+                    && songInfo.PlayQueueIndex == Data.MusicPlayer.PlayQueueIndex,
+                _ when !currentSong.IsOnline && songInfo is BriefLocalSongInfo localSong => (
+                    (BriefLocalSongInfo)currentSong
+                ).Path == localSong.Path
+                    && songInfo.PlayQueueIndex == Data.MusicPlayer.PlayQueueIndex,
+                _ => false,
+            };
+            playingFontIcon.Visibility = isCurrentlyPlaying
+                ? Visibility.Visible
+                : Visibility.Collapsed;
+        }
     }
 
     private void PlayButton_Click(object sender, RoutedEventArgs e)
