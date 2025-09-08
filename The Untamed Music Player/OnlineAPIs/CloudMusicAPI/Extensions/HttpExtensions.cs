45f4535b193a7ee6df08f1fdfdd1a723005d5142
LanZhan <WANGYIYI06300602@126.com>
2025-09-08 11:09:36 +0800

    测试

--- The Untamed Music Player/OnlineAPIs/CloudMusicAPI/Extensions/HttpExtensions.cs
+++ The Untamed Music Player/OnlineAPIs/CloudMusicAPI/Extensions/HttpExtensions.cs
@@ -2,13 +2,15 @@ namespace The_Untamed_Music_Player.OnlineAPIs.CloudMusicAPI.Extensions;
 
 internal static class HttpExtensions
 {
-    public static string ToQueryString(this IEnumerable<KeyValuePair<string, string>> queries)
+    extension(IEnumerable<KeyValuePair<string, string>> queries)
     {
-        ArgumentNullException.ThrowIfNull(queries);
-
-        return string.Join(
-            "&",
-            queries.Select(t => Uri.EscapeDataString(t.Key) + "=" + Uri.EscapeDataString(t.Value))
-        );
+        public string ToQueryString()
+        {
+            ArgumentNullException.ThrowIfNull(queries);
+            return string.Join(
+                "&",
+                queries.Select(t => Uri.EscapeDataString(t.Key) + "=" + Uri.EscapeDataString(t.Value))
+            );
+        }
     }
 }
