c06d31d0b03357bf7f1142b08d2eefc0ea38d146
LanZhan-Harmony <WANGYIYI06300602@126.com>
2026-04-06 20:43:46 +0800

    修复不扫描的bug

--- UntamedMusicPlayer/Helpers/NetworkHelper.cs
+++ UntamedMusicPlayer/Helpers/NetworkHelper.cs
@@ -4,16 +4,24 @@ public static class NetworkHelper
 {
     public static async Task<bool> IsInternetAvailableAsync()
     {
-        try
+        for (var i = 0; i < 3; i++)
         {
-            using var client = new HttpClient();
-            client.Timeout = TimeSpan.FromSeconds(5);
-            var response = await client.GetAsync("https://music.163.com");
-            return response.IsSuccessStatusCode;
+            try
+            {
+                using var client = new HttpClient();
+                client.Timeout = TimeSpan.FromSeconds(5);
+                var response = await client.GetAsync("https://music.163.com");
+                if (response.IsSuccessStatusCode)
+                {
+                    return true;
+                }
+            }
+            catch { }
+            if (i < 2)
+            {
+                await Task.Delay(300);
+            }
         }
-        catch
-        {
-            return false;
-        }
+        return false;
     }
 }
